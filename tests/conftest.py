0e09d396be25dcf2f96f017c7e63485447ddb22a
Miroslav Spousta <miroslav.spousta@rossum.ai>
2020-05-04 19:43:30 +0200

    Fix: Do not override global git config in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,6 @@ def empty_git_repo():
     git_dir = tempfile.mkdtemp()
     os.chdir(git_dir)
     run(['git', 'init'])
-    run(["git", "config", "--global", "user.name", "test"])
-    run(["git", "config", "--global", "user.email", "test@rossum.ai"])
+    run(["git", "config", "user.name", "test"])
+    run(["git", "config", "user.email", "test@rossum.ai"])
     return git_dir
